521b70de151e76a6cae32d7f815859db473dbf4d
agikerja <agibuatkerja@gmail.com>
2023-02-22 12:49:10 +0800

    progress variabel

---
+++ bin/belajardartt1.dart
@@ -0,0 +1,5 @@
+import 'package:belajardartt1/belajardartt1.dart' as belajardartt1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajardartt1.calculate()}!');
+}
---
+++ bin/variable.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+void main() {
+  //final
+  final name = 'agi';
+  print(name);
+  print(name);
+
+  print(name);
+  print(name);
+
+  //bedanya final dan const
+  final array1 = [1, 2, 3];
+  const array2 = [1, 2, 3];
+
+  array1[0] = 5;
+
+  print(array1);
+  print(array2);
+
+  //late
+  late var value =
+      getValue(); //karena belum dipanggil maka eksekusi ini diskip dulu dan dilanjutkan pada baris di bawahnya
+  print('variabel sudah dibuat');
+  print(
+      value); //karena variabel value sudah dipanggil maka seluruh method akan dieksekusi
+}
+
+String getValue() {
+  print('getValue() dipanggil');
+  return 'agi';
+}
